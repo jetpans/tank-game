d7bacf094208f27bb833db297fcdf86d34cbf9e3
Stjepan Đelekovčan <stjepan.djelekovcan@gmail.com>
2023-12-11 16:31:55 +0100

    Operators for breeding and mutation of genomes

--- python/Cartesian GP/CGPModel.py
+++ python/Cartesian GP/CGPModel.py
@@ -5,6 +5,11 @@ from util import random_geometric
 
 GENERATION_INDEX_PROBABILITY = 1/2
 
+#Best not to put this bigger than 1/3
+MUTATION_OF_ELEMENT_PROBABILITY = 1/36
+
+REPRODUCTION_PROBABILITY = 1/4
+
 #Returns a genome as a list of nodes of which, each is an uređena trojka (in1, in2, operator)
 #In1 and In2 are indices in a given genome
 #For In1 and In2 at index I, it is True that In1 < I && In2 < I
@@ -25,9 +30,6 @@ def construct_random_genome(number_of_nodes, number_of_inputs, constant_range):
     
     return genome_result
 
-myGenome = construct_random_genome(6, 2, 15)
-
-
 def evaluate_genome(genome, inputs):
     evaluation = [0]*len(genome)
     for i in range(len(genome)):
@@ -40,6 +42,35 @@ def evaluate_genome(genome, inputs):
         
 
     return evaluation
+
+def mutate_genome(genome):
+    for i in range(len(genome)):
+        for part in range(3):
+                rng = random.random()
+                if rng < MUTATION_OF_ELEMENT_PROBABILITY:
+                    if part < 2:
+                        newInput = i-random_geometric(i, GENERATION_INDEX_PROBABILITY)
+                        genome[i][part] = newInput
+                    else:
+                        newOperator = random.choice(list(operators.keys()))
+                        genome[i][part] = newOperator
+
+#Heuristika: bolje krizati krajnje elemente genoma
+def reproduce_genomes(genome1, genome2):
+    if len(genome1) < len(genome2):
+        genome1, genome2 = genome2, genome1
+        
+    c2 = genome2[::]
+    c1 = genome1[::]
+    for i in range(len(genome2)):
+        rng = random.randoom()
+        if rng < REPRODUCTION_PROBABILITY / (len(genome2) - i):
+            c2[i] = genome1[i]
+            c1[i] = genome2[i]
+            
+    return (c1,c2)
+
+myGenome = construct_random_genome(6, 2, 15)
 
 evaluation = evaluate_genome(myGenome, [1,3])
 
